06d010665d124bdac2067d199131642af4278f32
Lee Symes <leesdolphin@gmail.com>
2015-05-25 23:05:06 +1200

    Streaming now worksgit add .git add .

--- bugeye/server.py
+++ bugeye/server.py
@@ -1,6 +1,9 @@
 __author__ = 'lee'
 
 import asyncio
+
+asyncio._DEBUG = True
+import os
 from aiohttp import web
 import json
 from bugeye.time import midnight_time
@@ -25,6 +28,15 @@ class ServeStaticRoute(web.StaticRoute):
             url = self._prefix + filename
         return self._append_query(url, query)
 
+    @asyncio.coroutine
+    def handle(self, request):
+        try:
+            return (yield from super().handle(request))
+        except web.HTTPNotFound as e:
+            filename = request.match_info['filename']
+            print("Accessing: ", filename, "; ", os.path.join(self._directory, filename))
+            raise
+
 class LiveMixing(object):
 
     def __init__(self):
@@ -38,7 +50,7 @@ class LiveMixing(object):
     @asyncio.coroutine
     def room_config_handler(self, request):
         response = web.Response()
-        room = self.getRoom(request.match_info["room"])
+        room = self.get_room(request.match_info["room"])
         if room is None:
             response.set_status(404)
         else:
@@ -54,7 +66,7 @@ class LiveMixing(object):
     @asyncio.coroutine
     def change_mixer(self, request):
         response = web.Response()
-        room = self.getRoom(request.match_info["room"])
+        room = self.get_room(request.match_info["room"])
         if room is None:
             response.set_status(404)
         else:
@@ -68,30 +80,92 @@ class LiveMixing(object):
     @asyncio.coroutine
     def set_notes(self, request):
         response = web.Response()
-        room = self.getRoom(request.match_info["room"])
+        room = self.get_room(request.match_info["room"])
         if room is None:
             response.set_status(404)
         else:
             try:
-                talk_id = response.POST['talk-id']
-                talk_start = response.POST['talk-begin']
-                talk_end = response.POST['talk-end']
-                title = response.POST['title']
-                presenter = response.POST['presenter']
-                comments = response.POST['comments']
+                post_data = yield from response.post()
+                talk_id = post_data['talk-id']
+                talk_start = post_data['talk-begin']
+                talk_end = post_data['talk-end']
+                title = post_data['title']
+                presenter = post_data['presenter']
+                comments = post_data['comments']
             except KeyError as e:
                 raise web.HTTPBadRequest() from e
 
 
         return response
 
-    def getRoom(self, name):
+    @staticmethod
+    def get_room(self, name):
         print("Searching for", name)
         room_name = name.lower()
         for room in rooms:
             if room.room.lower() == room_name:
                 return room
         return None
+
+
+class Streaming(object):
+
+    def __init__(self):
+        pass
+
+    def init_routes(self, app):
+        app.router.add_route('GET', '/video.mkv', self.stream_test)
+        pass
+
+    @asyncio.coroutine
+    def stream_test(self, request):
+        print("Testing Stream: ", request)
+        response = web.StreamResponse()
+        chunk_size = 512
+        import mimetypes
+        content_type, encoding = mimetypes.guess_type("video.mp4")
+        print("Testing Stream: ", request, " || ", content_type, encoding)
+        response.enable_chunked_encoding()
+        response.content_type = content_type
+        response.start(request)
+        yield from self.stream_file(open("bugeye/video.mp4", 'rb'), response, chunk_size)
+        return response
+
+    @asyncio.coroutine
+    def stream_file(self, input_stream, started_response, chunk_size=512):
+        def get_chunk():
+            """
+
+            :return: The next chunk of length `chunk_size`.
+            """
+            import time
+            time.sleep(0.01)
+            ## Caution: This function is run in a thread!
+            return input_stream.read(chunk_size) or None
+        print('Starting to stream file')
+        yield from self.supply_chunks(get_chunk, started_response)
+        print('Done streaming file')
+
+    @staticmethod
+    @asyncio.coroutine
+    def supply_chunks(get_chunk_fn, output):
+        """
+
+        :param get_chunk_fn: A function taking no arguments and returning the next chunk(a type that can be
+                             passed into `output.write()` or None(indicating EOF)
+        :type get_chunk_fn: function()
+        :param output: A output stream that will have chunks of data written to. This should be non-blocking
+        :return: `False` if the end of the input stream indicated. `True` otherwise.
+        """
+        while True:
+            chunk = yield from asyncio.get_event_loop().run_in_executor(None, get_chunk_fn)
+            if chunk is not None:
+                output.write(chunk)
+            else:
+                break;
+
+
+
 
 rooms = [Mixer("hi", None, None, None)]
 
@@ -103,6 +177,9 @@ def init(loop):
     app.router.register_route(ServeStaticRoute("Static Content", "/static/", "bugeye/static/"))
     live = LiveMixing()
     live.init_routes(app)
+
+    streamer = Streaming()
+    streamer.init_routes(app)
 
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 8080)
     print("Server started at http://127.0.0.1:8080")
