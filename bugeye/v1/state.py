28398b471b9560dbf91e5c7171366a396036ccb0
Lee Symes <leesdolphin@gmail.com>
2015-06-22 09:35:18 +1200

    testing3

--- bugeye/v1/state.py
+++ bugeye/v1/state.py
@@ -2,9 +2,28 @@
 
 class MixerWrapper(object):
 
+    SUPPORTED_STATES = frozenset(['audio', 'video', 'pip'])
+
     def __init__(self, loop, mixer):
         self._loop = loop
         self._mixer = mixer
+        self._state = {}
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, new_state):
+        state = {}
+        for s in self.SUPPORTED_STATES:
+            ## Get or default. This also does an implicit copy
+            state[s] = int(new_state.get(s, 0))
+        self._state = state
+        self._mixer.set_mix(self._state)
+
+    
 
 
-    def
+
+
